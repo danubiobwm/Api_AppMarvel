a0bd04c735542a051424d97f7a0de000c7515e19
Klayton leandro <klayton_leandro54@hotmail.com>
2019-12-03 10:54:58 -0300

    testing jest config prettier SRC or __test__

---
+++ src/App-test-details.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+
+import FlatList from '../src/pages/Details';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('Testing...', () => {
+  renderer.create(<FlatList />);
+});
